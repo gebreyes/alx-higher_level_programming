12981ea1f1f65cc0934e6afac78889bf21327ca1
gebreyes <gebreyesis56@gmail.com>
2022-06-28 15:10:28 +0300

    ASCI in lowercase except e and q

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 for a in range(97, 123):
-    if a != 101 and a != 113:
-        print(chr(a), end="")
+    if a != 101 and a != 113: 
+        print("{}".format(chr(a)), end="")
+
