ed875a6766067aa4656ea2393a43d63e3a5ed442
Gebreyes Gebeyehu <gebreyesis56@gmail.com>
2022-06-28 16:26:14 +0300

    printing 0-99 in double format

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 for x in range(0, 100):
-    print("{:02d}".format(x), end=", "))
+    if x == 99:
+        print("{}".format(x))
+    else:
+        print("{:02d}".format(x), end=", ")
