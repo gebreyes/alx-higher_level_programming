3dfddaf15522ba7d653704c1ecbef56219386269
Gebreyes Gebeyehu <gebreyesis56@gmail.com>
2022-06-28 19:01:55 +0300

    fizzbuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for i in range(1, 102):
+    for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz ", end="")
         elif i % 3 == 0:
@@ -8,4 +8,4 @@ def fizzbuzz():
         elif i % 5 == 0:
             print("Buzz ", end="")
         else:
-            print(i, " " ,end="")
+            print(i, " ", end="")
