1f84fdcc75b713ef9f7c4968dde21d81fddd2885
Gebreyes Gebeyehu <gebreyesis56@gmail.com>
2022-06-30 21:31:49 +0300

    loading single variable value

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
-"""calling the value of a from variable_load_y.py python file"""
+
 if __name__ == "__main__":
+    """Displaying the value of a from variable_load_5.py"""
     from variable_load_5 import a
+
     priint(a)
