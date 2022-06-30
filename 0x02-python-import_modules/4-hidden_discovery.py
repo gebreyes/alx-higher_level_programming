87d4d913ca7d94eaa6f839a77453a581a0220d19
Gebreyes Gebeyehu <gebreyesis56@gmail.com>
2022-06-30 19:02:54 +0300

    finding hidden file

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import hidden_4
-    for l in dir(hidden_4):
-        if l[0:2] != "__":
-            print(l)
 
+if __name__ == "__main__":
+    """Print all names defined by hidden_4 module."""
+    import hidden_4
+
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
