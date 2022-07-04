8d72b158d208810878139dff7d7afd0e1583776f
Gebreyes Gebeyehu <gebreyesis56@gmail.com>
2022-07-04 15:45:22 +0300

    returning character from specific position

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,8 +3,8 @@
 
 def element_at(my_list, idx):
     if idx < 0:
-        return None
+        return 'None'
     elif idx > len(my_list) - 1:
-        return None
+        return 'None'
     else:
-        print("Element at index {:d}".format(my_list[idx]))
+        print("Element at index {:d} is {}".format(idx, my_list[idx]))
