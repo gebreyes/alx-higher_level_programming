e436885462d8a005e7aebac6e26c43c6c63c2473
Gebreyes Gebeyehu <gebreyesis56@gmail.com>
2022-07-04 16:05:53 +0300

    replace old list with new list

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -6,7 +6,5 @@ def replace_in_list(my_list, idx, element):
     elif idx > len(my_list) - 1:
         return my_list
     else:
-        for x in my_list:
-            my_list.pop(idx)
-            my_list.append(idx, element)
-            return my_list
+        my_list[idx] = element
+        return my_list
