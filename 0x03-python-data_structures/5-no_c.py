e5499f9769fd62fee1560764fbac35fb1c15e446
Gebreyes Gebeyehu <gebreyesis56@gmail.com>
2022-07-04 16:38:04 +0300

    removing character from specified position

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-
 def no_c(my_string):
-    for x in my_string:
-        if x == 'c' or x == 'C':
-            return 'None'
-        else:
-            return x
+    new_string = my_string.translate({ord(i): None for i in 'cC'})
+    return new_string
