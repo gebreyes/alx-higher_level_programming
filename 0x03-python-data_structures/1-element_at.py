048b3d727a41dfb09169d2f711da9279aa34dfb0
Gebreyes Gebeyehu <gebreyesis56@gmail.com>
2022-07-04 15:42:15 +0300

    from specific position

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,10 @@
+#!usr/bin/python3
+
+
+def element_at(my_list, idx):
+    if idx < 0:
+        return None
+    elif idx > len(my_list) - 1:
+        return None
+    else:
+        print("Element at index {:d}".format(my_list[idx]))
