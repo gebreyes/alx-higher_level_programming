fac3e78b176e448211f7d7f4667f18491bd34398
Gebreyes Gebeyehu <gebreyesis56@gmail.com>
2022-07-04 16:12:00 +0300

    printing in reverse order

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list):
-    # Syntax: reversed_list = systems[start:stop:step]
-    reversed_list = my_list[::-1]
-    return reversed_list
+    if my_list:
+        # Syntax: reversed_list = systems[start:stop:step]
+        reversed_list = my_list[::-1]
+        for x in range(len(reversed_list):
+                print("{:d}".format(reversed_list[x]))
