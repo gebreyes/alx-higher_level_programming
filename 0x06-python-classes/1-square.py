c7eb3b08ede7ee35b626677473bb4f39f1fcc957
Gebreyes Gebeyehu <gebreyesis56@gmail.com>
2022-07-19 23:23:48 +0300

    square class with one private instance attribute

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -4,7 +4,14 @@
 
 
 class Square:
-    """declare one private instance attribute"""
+    """Represent A square"""
+    
     def __init__(self, size):
+
+         """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
         self.__size = size
 
