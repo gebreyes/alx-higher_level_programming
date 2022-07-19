7910bd6c57b783113631d380385143d2bb505a56
Gebreyes Gebeyehu <gebreyesis56@gmail.com>
2022-07-20 00:02:32 +0300

    validation using setter method

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -24,7 +24,7 @@ class Square:
 
     @size.setter
     def size(self, value):
-        if not isdigit(value):
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
