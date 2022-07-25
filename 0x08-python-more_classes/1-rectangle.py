218a3ab28388092988cf945a56cc7423f0e73f82
gebreyes <gebreyesis56@gmail.com>
2022-07-25 22:52:58 +0300

    setting and getting width and height of rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-"""Rectangle Class declaration"""
+"""Defines a Rectangle class."""
 
 
 class Rectangle:
     """Represent a rectangle."""
 
     def __init__(self, width=0, height=0):
-
         """Initialize a new Rectangle.
 
         Args:
@@ -25,21 +24,19 @@ class Rectangle:
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
-        else:
-            self.__width = value
+        self.__width = value
 
     @property
-    def heigth(self):
-        """Get/set the heigth of the rectangle."""
-        return self.__heigth
+    def height(self):
+        """Get/set the height of the rectangle."""
+        return self.__height
 
-    @heigth.setter
-    def heigth(self, value):
+    @height.setter
+    def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
-        else:
-            self.__heigth = value
+        self.__height = value
